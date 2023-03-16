4e6becd392f3edaa82782ca0b56a2354878ba3f1
Moses <t.content012@gmail.com>
2023-03-16 23:27:22 +0300

    find

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+// const result = array.find((year)=>result==='w')
+
+// console.log(result)
+
+// const superbowlWin= function(record){
+//     let year = []
+//     for (let i=0; i<record.length; i++){
+//         if (record[i].result === 'W'){
+//             return record[i].year
+//         }
+//     }
+//     return null
+// }
+// console.log(superbowlWin(record))
+
+
+
+// function superbowlWin(record) {
+//     const winningRecord = record.find(record => record.result === "W");
+//     if (winningRecord) {
+//       return winningRecord.year;
+//     } else {
+//       return undefined;
+//     }
+//   }
+//   console.log(superbowlWin(records))
+
+function superbowlWin(record) {
+    const win = record.find(obj => obj.result === 'W');
+    return win ? win.year : undefined;
+  }
+  
